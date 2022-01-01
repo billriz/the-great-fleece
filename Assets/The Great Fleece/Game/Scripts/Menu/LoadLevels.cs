dfe498b6817d2756de8ffaed4768cce3e912a257
Bill Rislov <bill.rislov@gmail.com>
2022-01-01 15:18:44 -0600

    Main Menu completed

--- Assets/The Great Fleece/Game/Scripts/Menu/LoadLevels.cs
+++ Assets/The Great Fleece/Game/Scripts/Menu/LoadLevels.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoadLevels : MonoBehaviour
 {
+
+    public Image loadingBar;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(LoadLevelAsync());
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    IEnumerator LoadLevelAsync()
     {
         
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+
+        while (operation.isDone == false)
+        {
+
+            loadingBar.fillAmount = operation.progress;
+            yield return new WaitForEndOfFrame();
+        }
+
     }
 }
--- Assets/The Great Fleece/Game/Scripts/Menu/MainMenu.cs
+++ Assets/The Great Fleece/Game/Scripts/Menu/MainMenu.cs
@@ -20,7 +20,7 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
 
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("Loading_Screen");
         
     }
 
